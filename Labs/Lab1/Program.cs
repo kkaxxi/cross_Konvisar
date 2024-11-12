ebbc2818e49fa7902d4f177630ba9e51512e42fb
Konvisar Karyna <konvisar_karina@knu.ua>
2024-11-12 23:19:16 +0200

    Implemented the solution in the Chinese game

--- Labs/Lab1/Program.cs
+++ Labs/Lab1/Program.cs
@@ -1,8 +1,44 @@
-﻿namespace Lab1;
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello, World!");
+        string[] inputLines = File.ReadAllLines("INPUT.TXT");
+        int n = int.Parse(inputLines[0]); 
+
+        List<int> queries = new List<int>();
+        for (int i = 1; i <= n; i++)
+        {
+            queries.Add(int.Parse(inputLines[i]));
+        }
+
+        List<(int, int)> losingPairs = GenerateLosingPairs(queries.Max());
+
+        using (StreamWriter writer = new StreamWriter("OUTPUT.TXT"))
+        {
+            foreach (int k in queries)
+            {
+                (int a, int b) = losingPairs[k];
+                writer.WriteLine($"{a} {b}");
+            }
+        }
+    }
+
+    static List<(int, int)> GenerateLosingPairs(int maxK)
+    {
+        List<(int, int)> losingPairs = new List<(int, int)>();
+        double phi = (1 + Math.Sqrt(5)) / 2;  
+
+        for (int k = 0; k <= maxK; k++)
+        {
+            int a = (int)(k * phi);
+            int b = a + k;
+            losingPairs.Add((a, b));
+        }
+
+        return losingPairs;
     }
 }
